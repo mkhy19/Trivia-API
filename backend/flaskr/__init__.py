ffb23e77f8c2c8d2aee2224d1a3fc653db8eca21
mkhy19 <mkhy88519@gmail.com>
2020-10-15 04:24:34 +0200

    add pagination method

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,15 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
